e1300806c9d6444208ce7fc7e3680dcebd8639c6
Felix Clack <felixclack@gmail.com>
2012-02-20 22:16:41 +0000

    Remove dummy app

--- test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'bea3eb1f87dbad1fae11fff38597a2edcfd2cb2657e9a867c6eeb6ba8b316ea671f72d309b9eb28978b7645b8de92800667df52f9ef6c91c4ddd9ea0a920ed1f'
--- test/timeline_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class TimelineTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Timeline
-  end
-end
