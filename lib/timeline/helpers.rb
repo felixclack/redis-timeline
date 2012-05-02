259522fa0d133606a08ac6d500d2175cefa3c6fb
Jason Lee <huacnlee@gmail.com>
2012-05-02 16:30:06 +0800

    Fix MultiJson version bug;

--- lib/timeline/helpers.rb
+++ lib/timeline/helpers.rb
@@ -1,16 +1,26 @@
+require 'multi_json/version'
+
 module Timeline
   module Helpers
     class DecodeException < StandardError; end
 
     def encode(object)
-      ::MultiJson.dump(object)
+      if ::MultiJson::VERSION.to_f > 1.3
+        ::MultiJson.dump(object)
+      else
+        ::MultiJson.encode(object)
+      end
     end
 
     def decode(object)
       return unless object
 
       begin
-        ::MultiJson.load(object)
+        if ::MultiJson::VERSION.to_f > 1.3
+          ::MultiJson.load(object)
+        else
+          ::MultiJson.decode(object)
+        end
       rescue ::MultiJson::DecodeError => e
         raise DecodeException, e
       end
